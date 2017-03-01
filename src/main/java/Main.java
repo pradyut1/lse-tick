73501b413daff266b968518fdf8c876da1843469
Shameera Rathnayaka <shameerainfo@gmail.com>
2017-02-28 22:22:52 -0500

    laod symbols from file and write results to a CSV file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,12 @@
  *
  */
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -29,6 +35,7 @@ import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,14 +48,45 @@ public class Main {
     private static String TICK = "tick";
     private Set<String> accumulator = new HashSet<>();
     private static int COUNT = 0;
+    private static String symbolsFilePath;
+    private static File symbolsFile;
+    private static String resultCSVFilePath;
+    private static File resultCSVFile;
+    private static String workingDir;
+    private BufferedWriter bw;
 
     private List<String> symbols;
     // (?<code>P),(?<symbol>[A-Z]+),(?<tickId>[0-9]+),(?<tick>[0-9]*),
     private final Pattern WATCH_PATTERN = Pattern.compile("(?<code>P),(?<symbol>[A-Z]+),(?<tickId>[0-9]+),(?<tick>[0-9]*),");
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    public Main(String[] inputs) throws IOException {
+        checkInputs(inputs);
+    }
 
     public static void main(String[] args) throws IOException {
-        Main main = new Main();
+        Main main = new Main(args);
         main.execute();
+    }
+
+    private void checkInputs(String[] inputs) throws IOException {
+        if (inputs == null || inputs.length != 2) {
+            throw new RuntimeException("Invalid inputs, use <jar> <symbols> <csvfile> ");
+        }
+
+        workingDir = System.getProperty("user.dir");
+        symbolsFilePath = workingDir + File.separator + inputs[0];
+        symbolsFile = new File(symbolsFilePath);
+        if (!symbolsFile.exists()) {
+            throw new RuntimeException("Symbol file doesn't exist -> " + symbolsFilePath);
+        }
+
+        resultCSVFilePath = workingDir + File.separator + inputs[1];
+        resultCSVFile = new File(resultCSVFilePath);
+        if (resultCSVFile.createNewFile()) {
+            log.info("Result file created");
+        }
+        bw = new BufferedWriter(new FileWriter(resultCSVFile));
     }
 
     private void execute() throws IOException {
@@ -56,18 +94,18 @@ public class Main {
         System.out.println("Started");
         IQF = new IQFeed_Socket();
         if (IQF.ConnectSocket(IQF_PORT)) {
-            System.out.println("Connected to client");
+            log.info("Connected to client");
         } else {
-            System.out.println("Not connected :( ");
+            log.info("Not connected :( ");
         }
         IQF.CreateBuffers();
         setProtocol();
-        System.out.println("Message posted protocol set.");
+        log.info("Message posted protocol set.");
         Thread reader = new Thread(iqfReader);
         reader.start();
         setUpdateFields();
         ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService.scheduleAtFixedRate(schedule, 0, 1, TimeUnit.SECONDS);
+        scheduledExecutorService.scheduleAtFixedRate(this::watchSymbols, 0, 1, TimeUnit.SECONDS);
 //        getAllUpdateMessage();
 //        getTick(DEFAULT_SYMBOL);
     }
@@ -80,7 +118,15 @@ public class Main {
 
     private List<String> loadSymbols() {
         List<String> codes = new ArrayList<>();
-        codes.add(DEFAULT_SYMBOL);
+        try (BufferedReader br = new BufferedReader(new FileReader(symbolsFile))) {
+          String line = null;
+            while ((line = br.readLine()) != null) {
+                codes.add(line.trim().toUpperCase());
+            }
+        } catch (IOException e) {
+            log.severe("Error loading symbols");
+        }
+        log.info("Loaded " + codes.size() + " symbols");
         return codes;
     }
 
@@ -100,8 +146,9 @@ public class Main {
             IQF.brBufferedWriter.write(command);
             IQF.brBufferedWriter.flush();
         } catch (IOException e) {
-            System.out.println(errorMsg);
+            log.severe(errorMsg);
         }
+
     }
 
     private void getTick(String symbol){
@@ -110,10 +157,9 @@ public class Main {
     }
 
 
-
-    private Runnable schedule = () -> {
+    private void watchSymbols() {
         symbols.forEach(this::watch);
-    };
+    }
 
     private void watch(String symbol) {
         String command = "w" + symbol + "\r\n";
@@ -129,7 +175,7 @@ public class Main {
                 parseForWatch(line).ifPresent(this::sum);
             }
         } catch (IOException e) {
-            System.out.println("Error while reading IQFeed");
+            log.severe("Error while reading IQFeed");
         }
     };
 
@@ -143,8 +189,16 @@ public class Main {
         COUNT += res.getTickValue();
     }
 
-    private void sendOutput(int count) {
-        System.out.printf("[Time-%d] Count is: %d%n", Calendar.getInstance().getTime().getSeconds(), count);
+    private void sendOutput(int count){
+        String str = buildOutput(getDateAndTime(), count);
+        try {
+            bw.newLine();
+            bw.write(str);
+            bw.flush();
+            log.info(str);
+        } catch (IOException e) {
+            log.severe("Error writing  to CSV file, -> " + str);
+        }
     }
 
     Optional<Result> parseForWatch(String line) {
@@ -154,4 +208,24 @@ public class Main {
         }
         return Optional.empty();
     }
+
+    private String buildOutput(String dateAndTime, int count) {
+        return new StringBuilder(dateAndTime)
+                .append(",")
+                .append(count)
+                .toString();
+    }
+
+    private String getDateAndTime(){
+        Calendar instance = Calendar.getInstance();
+        return new StringBuilder(instance.get(Calendar.YEAR))
+                .append(instance.get(Calendar.MONTH))
+                .append(instance.get(Calendar.DATE))
+                .append(",")
+                .append(instance.get(Calendar.HOUR))
+                .append(instance.get(Calendar.MINUTE))
+                .append(instance.get(Calendar.SECOND))
+                .toString();
+    }
+
 }
